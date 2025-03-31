291c7da25f6ceea370e591e88e325dd40653c8e3
RobinHood0101 <robinkammer@proton.me>
2025-03-31 11:07:01 +0200

    implement canvas1 and canvas2
    - add vue project with p5js

---
+++ creativeCoding/src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <header>
+
+        <div class="wrapper">
+
+            <nav>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+                <RouterLink to="/canvas1">Canvas1</RouterLink>
+                <RouterLink to="/canvas2">Canvas2</RouterLink>
+                <RouterLink to="/canvas3">Canvas3</RouterLink>
+                <RouterLink to="/canvas4">Canvas4</RouterLink>
+            </nav>
+        </div>
+    </header>
+
+    <RouterView />
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ creativeCoding/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script>
+import P5Canvas2 from '../components/P5Canvas2.vue';
+
+export default {
+    components: {
+        P5Canvas2
+    },
+    data() {
+        return {
+            count: 0
+        }
+    }
+}
+</script>
+
+<template>
+    <main>
+        home
+        <P5Canvas2 />
+    </main>
+</template>
