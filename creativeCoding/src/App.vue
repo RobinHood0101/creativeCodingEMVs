32d8ee4951199cce87767621aed6d657bcc5b441
RobinHood0101 <robinkammer@proton.me>
2025-04-28 10:54:44 +0200

    small changes
    - syntax
    - css

--- creativeCoding/src/App.vue
+++ creativeCoding/src/App.vue
@@ -1,26 +1,23 @@
 <script>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
     <header>
-
         <div class="wrapper">
-
-            <nav>
-                <ul class="flex gap-4">
+            <nav class="fixed top-0 bg-blue-300 p-1">
+                <ul class="flex gap-4 underline">
                     <li><RouterLink to="/">Home</RouterLink></li>
                     <li><RouterLink to="/patterns">Patterns</RouterLink></li>
                     <li><RouterLink to="/image-manipulation">Image Manipulation</RouterLink></li>
+                    <li><RouterLink to="/agent">Agent</RouterLink></li>
+                    <li><RouterLink to="/l-systems">L-Systems</RouterLink></li>
                 </ul>
             </nav>
         </div>
     </header>
 
     <RouterView />
-
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
