2e28385068a3fc28a48635f61588d27e567e4909
RobinHood0101 <robinkammer@proton.me>
2025-04-28 10:54:10 +0200

    Create AgentView.vue

---
+++ creativeCoding/src/views/AgentView.vue
@@ -0,0 +1,52 @@
+<script>
+import p5 from "p5";
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    mounted() {
+        this.createCanvas();
+    },
+    beforeDestroy() {
+        if (this.p) {
+            this.p.remove();
+        }
+    },
+    methods: {
+        createCanvas() {
+            this.p = new p5(this.sketch, this.$refs.canvasContainer);
+        },
+        sketch(p) {
+            p.preload = () => {
+            };
+            p.setup = () => {
+                p.createCanvas(400, 400);
+            };
+            p.draw = () => {
+                p.background(200)
+                for (let i = 0; i < p.width; i+=4) {
+                    p.noFill()
+                    p.square(0, 0, i)
+                }
+                p.fill(200);
+                p.circle(p.width/2, p.height/2, 30);
+            };
+            p.mousePressed = () => {
+
+            };
+        }
+    }
+}
+
+</script>
+
+
+<template>
+    <h1>Agent</h1>
+    <div ref="canvasContainer"></div>
+</template>
+
+
+<style scoped></style>
