2dda85a07c31b7494a320ac8b82dd6faa787da4d
RobinHood0101 <robinkammer@proton.me>
2025-05-21 11:30:23 +0200

    fix conways algorithm

--- creativeCoding/src/views/ConwaysGameView.vue
+++ creativeCoding/src/views/ConwaysGameView.vue
@@ -4,7 +4,30 @@ export default {
     name: "conwaysGame",
     data() {
         return {
-            newGame: [],
+            currentGame: [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ],
+            start: false,
+            speed: 10,
         };
     },
     mounted() {
@@ -25,38 +48,40 @@ export default {
             };
 
             p.draw = () => {
-                // test
-                //
-                let currentGame = [];
-                if (this.newGame.length > 0) {
-                    currentGame = this.newGame;
-                } else {
-                    currentGame = [
+                if (!this.start) {
+                    return;
+                }
+                p.frameRate(this.speed);
+                // init game if no entry
+                if (!this.currentGame.length > 0) {
+                    this.currentGame = [
                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                     ];
                 }
-
+                // Make sure nextGame is not a Reference
+                let nextGame = this.currentGame.map((row) => row.slice());
                 let indexY = 0;
                 let indexX = 0;
+
                 // Vertical
                 for (let y = 0; y < p.height; y += 20) {
                     // Horizontal
@@ -65,31 +90,29 @@ export default {
                             indexX = 0;
                         }
 
-                        // Alive cell
-                        if (currentGame[indexY][indexX] === 1) {
+                        if (nextGame[indexY][indexX] === 1) {
+                            // Alive cell
                             p.fill(100);
                             p.square(x, y, 20);
-                            p.fill(255);
                             indexX++;
-                            continue;
+                        } else {
+                            // Dead cell
+                            p.fill(255);
+                            p.square(x, y, 20);
+                            indexX++;
                         }
 
-                        // Dead cell
-                        p.fill(255);
-                        p.square(x, y, 20);
-                        indexX++;
-
                         // Check
-                        let currentCell = currentGame[indexY][indexX];
+                        let currentCell = nextGame[indexY][indexX];
 
-                        let neighbour1 = this.getCell(indexY - 1, indexX - 1, currentGame);
-                        let neighbour2 = this.getCell(indexY - 1, indexX, currentGame);
-                        let neighbour3 = this.getCell(indexY - 1, indexX + 1, currentGame);
-                        let neighbour4 = this.getCell(indexY, indexX + 1, currentGame);
-                        let neighbour5 = this.getCell(indexY + 1, indexX + 1, currentGame);
-                        let neighbour6 = this.getCell(indexY + 1, indexX, currentGame);
-                        let neighbour7 = this.getCell(indexY + 1, indexX - 1, currentGame);
-                        let neighbour8 = this.getCell(indexY, indexX - 1, currentGame);
+                        let neighbour1 = this.getCell(indexY - 1, indexX - 1, this.currentGame);
+                        let neighbour2 = this.getCell(indexY - 1, indexX, this.currentGame);
+                        let neighbour3 = this.getCell(indexY - 1, indexX + 1, this.currentGame);
+                        let neighbour4 = this.getCell(indexY, indexX + 1, this.currentGame);
+                        let neighbour5 = this.getCell(indexY + 1, indexX + 1, this.currentGame);
+                        let neighbour6 = this.getCell(indexY + 1, indexX, this.currentGame);
+                        let neighbour7 = this.getCell(indexY + 1, indexX - 1, this.currentGame);
+                        let neighbour8 = this.getCell(indexY, indexX - 1, this.currentGame);
 
                         let neighbours = [
                             neighbour1,
@@ -104,34 +127,42 @@ export default {
 
                         // loop over cells
                         let neighbourCount = 0;
-                        for (let neighbour of neighbours) {
+                        for (const neighbour of neighbours) {
                             if (neighbour === 1) {
                                 neighbourCount++;
                             }
                         }
+                        /*                         p.fill(0);
+                        p.textAlign();
+                        p.textSize(16);
+                        p.text(neighbourCount, x + 7, y + 15); */
+
                         //Check 1: Zelle hat weniger als 2 nachbar
-                        if (neighbourCount <= 2) {
-                            currentGame[indexY][indexX] = 0;
+                        if (neighbourCount < 2) {
+                            nextGame[indexY][indexX] = 0;
                         }
 
                         // Check 2:
-                        if (neighbourCount === 2 || (neighbourCount === 3 && currentCell === 1)) {
-                            currentGame[indexY][indexX] = 1;
+                        if (neighbourCount === 2 || neighbourCount === 3) {
+                            if (currentCell === 1) {
+                                nextGame[indexY][indexX] = 1;
+                            }
                         }
 
                         // Check 3:
                         if (neighbourCount > 3) {
-                            currentGame[indexY][indexX] = 0;
+                            nextGame[indexY][indexX] = 0;
                         }
 
                         // Check 4:
                         if (neighbourCount === 3 && currentCell === 0) {
-                            currentGame[indexY][indexX] = 1;
+                            nextGame[indexY][indexX] = 1;
                         }
                     }
                     indexY++;
                 }
-                this.newGame = currentGame;
+                this.currentGame = nextGame;
+                //console.log(this.currentGame);
             };
 
             p.mousePressed = () => {
@@ -144,6 +175,37 @@ export default {
             }
             return 0;
         },
+        toggleGame() {
+            this.start = !this.start;
+            console.log("tset");
+        },
+        clearGame() {
+            this.currentGame = [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            ];
+        },
+        toggleCell(y, x, event) {
+            this.currentGame[y][x] = event.target.checked ? 1 : 0;
+        },
     },
 };
 </script>
@@ -152,5 +214,21 @@ export default {
     <main>
         <h1>Conways Game of Life</h1>
         <div ref="conwaysGame"></div>
+        <button class="bg-sky-300 m-4 p-1 rounded hover:scale-105" id="controlBtn" @click="toggleGame">
+            {{ start ? "Stop" : "Start" }}
+        </button>
+        <button class="bg-sky-300 p-1 rounded hover:scale-105" id="clearBtn" @click="clearGame">Clear</button>
+        <input type="range" v-model="speed" min="1" max="20" />
+        <div id="checkboxGrid">
+            <div v-for="(row, y) in currentGame" :key="y" class="row">
+                <input
+                    v-for="(cell, x) in row"
+                    :key="x"
+                    type="checkbox"
+                    :checked="cell === 1"
+                    @change="toggleCell(y, x, $event)"
+                />
+            </div>
+        </div>
     </main>
 </template>
